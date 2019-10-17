d267f3d61b047c0b29fa0a51b7b29d9dbd4428ba
Naochi <n.shiba0101@gmail.com>
2019-10-17 19:21:13 +0900

    お願いします

---
+++ test2.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> 
+using namespace std;
+#define rep(i, n) for(ll i=0; i<n; i++)
+#define Rep(i, j, n) for(int i=j; i<n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+int main() {
+    cout << "Hello, World!" << endl;
+}
